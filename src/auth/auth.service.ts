c1122ab658e5aeb9cd288e5bcaf7e7c7b3bf7502
Dmitriy Afanasiev <afanasiev.dimon@outlook.com>
2023-01-19 23:25:25 +0100

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { msg: 'I am signed up' };
+  }
+  signin() {
+    return { msg: 'I am signed in' };
+  }
+}
